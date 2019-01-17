78e30fc60485dc49bf8f3fa54a164e5c41cf8042
Jio Mamaoag <jiomamaoag@gmail.com>
2019-01-17 17:14:13 +0800

    Update 05.cpp

--- 05.cpp
+++ 05.cpp
@@ -1,2 +1,2 @@
 #include <iostream>
-void display(char sym){for (int x = 0; x < 20; x++){printf("%c", sym);}std::cout << std::endl;}void display(char sym, int rep){for (int x = 0; x < rep; x++){printf("%c", sym);}std::cout << std::endl;}int main(){display('*');display('@'); display('*',10);display('#',15);}
+void display(char sym){for (int x = 0; x < 20; x++){printf("%c", sym);}std::cout << std::endl;} void display(char sym, int rep){for (int x = 0; x < rep; x++){printf("%c", sym);}std::cout << std::endl;}int main(){display('*');display('@'); display('*',10);display('#',15);}
